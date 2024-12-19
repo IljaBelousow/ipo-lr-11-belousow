5fedfabc714fe0afb89c789b0ab3d196cd7816c6
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-19 22:32:22 +0300

    Update van.py

--- transport/van.py
+++ transport/van.py
@@ -1,9 +1,4 @@
 from transport.vehicle import Vehicle
-from transport.van import Van
-from transport.airplane import Airplane
-from transport.client import Client
-from main import save_transports
-from main import save_clients
 
 class Van(Vehicle):
     def __init__(self, capacity, is_refrigerated):
