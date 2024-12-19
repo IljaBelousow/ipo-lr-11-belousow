e0038d81bf8051103b5f8064f03facc60e579d68
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-19 22:32:56 +0300

    Update airplane.py

--- transport/airplane.py
+++ transport/airplane.py
@@ -1,9 +1,5 @@
 from transport.vehicle import Vehicle
-from transport.van import Van
-from transport.airplane import Airplane
-from transport.client import Client
-from main import save_transports
-from main import save_clients
+
 
 class Airplane(Vehicle):
     def __init__(self, capacity, max_altitude):
